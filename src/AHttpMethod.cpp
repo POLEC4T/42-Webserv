165c87e96377796307785f528b056295320481f9
Milo Niemaz <mniemaz@z4r10p2.42lyon.fr>
2025-11-04 14:42:01 +0100

    rm prints

--- src/AHttpMethod.cpp
+++ src/AHttpMethod.cpp
@@ -6,7 +6,7 @@
 /*   By: mniemaz <mniemaz@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2025/11/04 14:07:17 by mniemaz          ###   ########.fr       */
+/*   Updated: 2025/11/04 14:41:51 by mniemaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -226,16 +226,13 @@ Response AHttpMethod::POST(std::string filename, Request& req, Server& serv)
 	
     if (stat(filename.c_str(), infos) != 0)
     {
-		std::cout << "File does not exist, will be created: " << filename << std::endl;
         if (infos && S_ISDIR(infos->st_mode))
         {
-			std::cout << "POST to a directory is forbidden: " << filename << std::endl;
             delete infos;
             return Response(req.getVersion(), serv.getErrorPageByCode(FORBIDDEN));
         }
     }
     delete infos;
-	std::cout << "Writing to file: " << filename << std::endl;
 	std::string directory = filename.substr(0, filename.find_last_of("/") + 1);
 	if (access(directory.c_str(), W_OK) != 0)
 		return Response(req.getVersion(), serv.getErrorPageByCode(FORBIDDEN));
---
+++ upload/sandbox.cpp
@@ -0,0 +1,295 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sandbox.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mniemaz <mniemaz@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/24 10:46:35 by mniemaz           #+#    #+#             */
+/*   Updated: 2025/11/04 14:41:12 by mniemaz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/socket.h>
+#include <netdb.h>
+#include <iostream>
+#include <unistd.h>
+#include <sstream>
+#include <fstream>
+#include <exception>
+#include <sys/epoll.h>
+#include <cstdlib>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define MAX_EVENT_WAITED 1024
+#define NB_EVENTS 1024
+#define BUFFER_SIZE 1024
+
+std::string my_to_string(int value) {
+    std::ostringstream oss;
+    oss << value;
+    return oss.str();
+}
+
+std::string getLocation(std::string &request) {
+    size_t pos = request.find(' ');
+    if (pos == std::string::npos) {
+		return "";
+	}
+	pos += 1;
+	size_t endPos = request.find(" ", pos);
+	if (endPos == std::string::npos) {
+		return "";
+	}
+	return request.substr(pos, endPos - pos);
+}
+
+std::string getMethod(std::string &request) {
+	size_t pos = request.find(' ');
+	if (pos == std::string::npos) {
+		return "";
+	}
+	return request.substr(0, pos);
+}
+
+std::string getHTMLPageResponse() {
+	std::ifstream htmlFile;
+	htmlFile.open("./src/index.html");
+	if (!htmlFile.is_open()) {
+		std::cerr << "Erreur : impossible d'ouvrir index.html" << std::endl;
+		throw std::exception();
+	}
+	std::stringstream content;
+	content << htmlFile.rdbuf();
+	std::string html = (content.str());
+	std::string response =
+		"HTTP/1.1 200 OK\r\n"
+		"Content-Type: text/html\r\n"
+		"Access-Control-Allow-Origin: *\r\n"
+		"Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\n"
+		"Access-Control-Allow-Headers: Content-Type, Authorization\r\n"
+		"Content-Length: " + my_to_string(html.size()) + "\r\n"
+		"\r\n" + html;
+	return response;
+}
+
+std::string getChatResponse(std::string &request) {
+	std::string body;
+	size_t pos = request.find("\r\n\r\n");
+	if (pos != std::string::npos) {
+		body = request.substr(pos + 4);
+	}
+	std::string response =
+		"HTTP/1.1 200 OK\r\n"
+		"Content-Type: application/json\r\n"
+		"Access-Control-Allow-Origin: *\r\n"
+		"Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\n"
+		"Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With\r\n"
+		"Content-Length: " + my_to_string(body.size()) + "\r\n"
+		"\r\n" + body;
+	return response;
+}
+
+std::string getOptionsResponse() {
+	std::string response =
+		"HTTP/1.1 200 OK\r\n"
+		"Access-Control-Allow-Origin: *\r\n"
+		"Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\n"
+		"Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With\r\n"
+		"Content-Length: 0\r\n"
+		"\r\n";
+	return response;
+}
+
+std::string getResponse(std::string &requestContent) {
+	std::string response;
+	std::string loc = getLocation(requestContent);
+	std::string method = getMethod(requestContent);
+	
+	std::cout << "Method: " << method << ", Location: " << loc << std::endl;
+	
+	if (method == "OPTIONS") {
+		response = getOptionsResponse();
+	}
+	else if (loc == "/" || loc == "/favicon.ico") {
+		try {
+			response = getHTMLPageResponse();
+		} catch (...) {
+			throw std::exception();
+		}
+	} else if (loc == "/message") {
+		response = getChatResponse(requestContent);
+	} else {
+		std::cerr << "404 not found" << std::endl;
+		response = 
+			"HTTP/1.1 404 Not Found\r\n"
+			"Access-Control-Allow-Origin: *\r\n"
+			"Content-Length: 0\r\n"
+			"\r\n";
+	}
+	
+	return response;
+}
+
+
+void readRequest(int clientFd, std::string &requestContent) {
+	// todo : read while end != /r/n/r/n
+	char buffer[BUFFER_SIZE] = { 0 };
+	int limit = 0;
+
+	ssize_t sizeRead;
+	while (1) {
+		if (limit > 10)
+		{
+			std::cerr << "limit reached " << std::endl;
+			break;
+		}
+		limit++;
+		sizeRead = recv(clientFd, buffer, BUFFER_SIZE, 0);
+		if (sizeRead == -1) {
+			std::cerr << "recv: error !" << std::endl;
+			break;
+		} else if (sizeRead == 0) {
+			std::cerr << "Client disconnected" << std::endl;
+			break;
+		}
+
+		requestContent.append(buffer, sizeRead);
+		if (requestContent.find("\r\n\r\n") != std::string::npos)
+			break;
+
+		
+	}
+}
+/**
+ * - Sets up a TCP server socket
+ * - Binds it to the specified address and port
+ * - Listens for incoming connections
+ */
+void setupServer(int &servFd, struct sockaddr_in &servAddr){
+	servFd = socket(AF_INET, SOCK_STREAM, 0);
+	if (servFd == -1) {
+		std::cerr << "socket err" << std::endl;
+		throw std::exception();
+	}
+	int opt = 1;
+	setsockopt(servFd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+	servAddr.sin_family = AF_INET;
+	servAddr.sin_port = htons(8080);
+	servAddr.sin_addr.s_addr = INADDR_ANY;
+		
+	if (bind(servFd, (struct sockaddr *)&servAddr, sizeof(servAddr)) == -1) {
+		std::cerr << "bind err" << std::endl;
+		throw std::exception();
+	}
+		
+	if (listen(servFd, 2) == -1) {
+		std::cerr << "listen err" << std::endl;
+		throw std::exception();
+	}
+}
+
+/**
+ * F_GETFL is FORBIDDEN
+ */
+int setNonBlocking(int sockfd) {
+	int flags = fcntl(sockfd, F_GETFL, 0); // change this line to not use F_GETFL
+	if (flags == -1) {
+		std::cerr << "fcntl(F_GETFL)" <<std::endl;
+		return -1;
+	}
+	if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1) {
+		std::cerr << "fcntl(F_SETFL)" <<std::endl;
+		return -1;
+	}
+	return 0;
+}
+
+int main(void) {
+	struct sockaddr_in servAddr;
+	int servFd;
+	try {
+		setupServer(servFd, servAddr);
+	} catch (...) {
+		return 1;
+	}
+
+	struct epoll_event ev;
+	struct epoll_event events[NB_EVENTS];
+
+	int epollFd = epoll_create(1);
+	if (epollFd == -1) {
+		std::cerr << "epoll_create err" << std::endl;
+		return (1);
+	}
+	setNonBlocking(servFd);
+	ev.events = EPOLLIN | EPOLLET;
+	ev.data.fd = servFd;
+	if (epoll_ctl(epollFd, EPOLL_CTL_ADD, servFd, &ev) == -1) {
+		std::cerr << "epoll_ctl: servFd" << std::endl;
+		return (1);
+	}
+		
+	int eventsReady;
+	while (1) {
+		printf("Waiting to accept!\n");
+
+		eventsReady = epoll_wait(epollFd, events, MAX_EVENT_WAITED, -1);
+		if (eventsReady == -1) {
+			std::cerr << "epoll_wait" << std::endl;
+			return (1);
+		} else {
+			std::cout << eventsReady << " event" << (eventsReady > 1 ? "s" : "") << " ready" << std::endl;
+		}
+		for (int i = 0; i < eventsReady; ++i) {
+			if (events[i].data.fd == servFd) {
+				struct sockaddr clientAddr;
+				int addrLen = sizeof(clientAddr);
+				int clientFd = accept(servFd, (struct sockaddr *)&clientAddr, (socklen_t *)&(addrLen));
+				if (clientFd == -1) {
+					std::cerr << "accept" << std::endl;
+					return (1);
+				}
+
+				setNonBlocking(clientFd);
+
+				ev.events = EPOLLIN | EPOLLET; //EPOLLET: epoll_wait() will only report an event once
+				ev.data.fd = clientFd;
+				if (epoll_ctl(epollFd, EPOLL_CTL_ADD, clientFd, &ev) == -1) {
+					std::cerr << "epollctl: clientFd" << std::endl;
+					return (1);
+				}
+				std::cout << "New connection, fd: " << clientFd << std::endl;
+			} else {
+				std::string response;
+				int clientFd = events[i].data.fd;
+				try {
+					std::string requestContent;
+					readRequest(clientFd, requestContent);
+					response = getResponse(requestContent);
+
+					if (send(clientFd, response.c_str(), response.size(), 0) == -1)
+						std::cout << "send: error !" << std::endl;
+
+					std::cout << "Response sent" << std::endl;
+				} catch (...) {
+					std::cerr << "Error while creating response" << std::endl;
+					epoll_ctl(epollFd, EPOLL_CTL_DEL, events[i].data.fd, NULL);
+					close(events[i].data.fd);
+					return (1) ;
+				}
+			}
+		}
+		std::cout << "-----\n";
+	}
+
+	epoll_ctl(epollFd, EPOLL_CTL_DEL, servFd, NULL);
+	close(servFd);
+
+	
+	return 0;
+}
+
+// todo: une requete peut arriver en plusieurs morceaux -> faire un buffer par client qui persiste
+// pareil pour send
